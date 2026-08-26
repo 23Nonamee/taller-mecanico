f174a0a99476530e6d983dc065dfcc312c4bd5f1
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-08-25 20:15:32 -0400

    add

--- vehiculo.py
+++ vehiculo.py
@@ -1,5 +1,24 @@
 class Vehiculo:
-    def __init__(self, patente: str, anio : int,):
+    def __init__(self, patente: str, anio : int, modelo: str):
         self.patente = patente
         self.anio = anio 
+        self.modelo = modelo
         self.__en_taller: bool = False
+
+    def ingresar(self) -> str :
+        if self.__en_taller: 
+            return "El vehiculo ya esta en el taller"
+        
+        self.__en_taller = True
+        return "El vehiculo ha sido ingresado"
+
+    def entregar(self) -> str:
+        if not self.__en_taller:
+            return "El vehiculo no esta en el taller"
+        self.__en_taller = False
+        return "El vehiculo ha sido entregado"
+    
+    def tarifa_hora(self) -> int:
+        return 5000
+     
+        
